8bf96f09149232c56bdb174b9779d371b08ff043
Bryan Powell <b.powell@salesforce.com>
2020-03-09 09:29:35 -0700

    move describe to scripts

--- scripts/metadata-registry/update.js
+++ scripts/metadata-registry/update.js
@@ -2,19 +2,22 @@ const fs = require('fs');
 const { join } = require('path');
 
 // Prep the registry
-const REGISTRY_FILE = '../src/metadata-registry/data/registry.json';
+const REGISTRY_PATH = join(
+  __dirname,
+  '..',
+  '..',
+  'src',
+  'metadata-registry',
+  'data',
+  'registry.json'
+);
 const EMPTY_REGISTRY = { types: {}, suffixes: {}, mixedContent: {} };
-const registryPath = join(__dirname, REGISTRY_FILE);
-const registry = fs.existsSync(registryPath)
-  ? JSON.parse(fs.readFileSync(registryPath))
+const registry = fs.existsSync(REGISTRY_PATH)
+  ? JSON.parse(fs.readFileSync(REGISTRY_PATH))
   : EMPTY_REGISTRY;
 
 // TODO: Replace with api call
-const describe = JSON.parse(
-  fs.readFileSync(
-    join(__dirname, '../src/metadata-registry/data/describe.json')
-  )
-);
+const describe = JSON.parse(fs.readFileSync(join(__dirname, 'describe.json')));
 
 for (const object of describe.metadataObjects) {
   const typeId = object.xmlName.toLowerCase();
@@ -42,6 +45,6 @@ for (const object of describe.metadataObjects) {
   }
 }
 
-fs.writeFileSync(registryPath, JSON.stringify(registry, null, 2));
+fs.writeFileSync(REGISTRY_PATH, JSON.stringify(registry, null, 2));
 
 console.log('Registry updated');

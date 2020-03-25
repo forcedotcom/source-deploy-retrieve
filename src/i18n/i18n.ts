23434a15b1786cd4ec017549f48e9683cee2ca6e
Jag <jjayaprakash@salesforce.com>
2020-03-25 15:45:22 -0700

    @W-6531281@ Addressing code review comments + Adding tests

--- src/i18n/i18n.ts
+++ src/i18n/i18n.ts
@@ -23,5 +23,9 @@ export const messages = {
   registry_error_file_not_found: 'File not found %s',
   registry_error_missing_metadata_xml: 'Metadata xml file missing for %s',
   registry_error_unsupported_type:
-    'Types missing a defined suffix are currently unsupported'
+    'Types missing a defined suffix are currently unsupported',
+  beta_tapi_mdcontainer_error: 'Unexpected error creating metadata container',
+  beta_tapi_membertype_error: 'Unexpected error creating %s member',
+  beta_tapi_car_error: 'Unexpected error creating container async request',
+  beta_tapi_queue_status: 'The deploy is still in the Queue'
 };

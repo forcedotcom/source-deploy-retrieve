bd24f7c48f87a4009c04ebcdfc9cafd58f2ab304
Jag Jayaprakash <58611665+jag-sfdc@users.noreply.github.com>
2020-03-30 13:26:08 -0700

    Fix the typo missed in the last merge resolution.

--- src/i18n/i18n.ts
+++ src/i18n/i18n.ts
@@ -28,7 +28,7 @@ export const messages = {
   beta_tapi_membertype_error: 'Unexpected error creating %s member',
   beta_tapi_car_error: 'Unexpected error creating container async request',
   beta_tapi_queue_status: 'The deploy is still in the queue',
-  beta_tapi_membertype_unsupported_error: '%s type not supported'
+  beta_tapi_membertype_unsupported_error: '%s type not supported',
   error_directories_not_supported:
     'Paths to directories are currently not supported',
   error_expected_source_files: "%s: Expected source files for '%s'",

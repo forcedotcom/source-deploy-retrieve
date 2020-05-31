b98b81552565760e029d8cdc2062ca9b15272adb
Bryan Powell <powell.bryan@outlook.com>
2020-05-31 02:28:49 -0700

    update messaging

--- scripts/verifyTestArtifacts.js
+++ scripts/verifyTestArtifacts.js
@@ -1,12 +1,12 @@
 const { existsSync } = require('fs');
 const { join } = require('path');
 const { terminalCodes } = require('./util');
-
 const { FgRed, FgWhite } = terminalCodes;
 
 const testResultsPath = join(process.cwd(), 'test-results', 'test-results.xml');
+
 if (!existsSync(testResultsPath)) {
-  const message = `${FgRed}Test artifacts missing!\nExpected ${testResultsPath} to exist\n\n${FgWhite}The mocha test suite failed to run properly. Possible issues may be circular dependencies or an incompatible dependecy.`;
+  const message = `${FgRed}Test artifacts missing! Expected ${testResultsPath} to exist. The mocha test suite failed to run properly - Possible issues may be circular dependencies or an incompatible dependency.`;
   console.error(`${FgRed}${message}`);
   process.exit(1);
 }

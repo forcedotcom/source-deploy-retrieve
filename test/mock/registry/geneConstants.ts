99590acb97afb997716cf518cf3cd6c481a8b9b2
Bryan Powell <b.powell@salesforce.com>
2020-06-23 14:13:06 -0700

    ESlint enforce copyright header w/ Husky pre-commit (#69)

--- test/mock/registry/geneConstants.ts
+++ test/mock/registry/geneConstants.ts
@@ -1,10 +1,10 @@
-import { mockRegistry } from '.';
 /*
  * Copyright (c) 2020, salesforce.com, inc.
  * All rights reserved.
  * Licensed under the BSD 3-Clause license.
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
+import { mockRegistry } from '.';
 import { join } from 'path';
 import { MetadataComponent } from '../../../src/types';
 

a3c606038e0685293f69e18e76064c81280d39f1
Bryan Powell <b.powell@salesforce.com>
2020-08-04 12:55:02 -0700

    fix build issues

--- src/convert/convertTransaction.ts
+++ src/convert/convertTransaction.ts
@@ -5,7 +5,7 @@
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 import { WriterFormat } from '../types';
-import { DecomposedMetadataTransformer } from './transformers/DecomposedMetadataTransformer';
+import { DecomposedMetadataTransformer } from './transformers/decomposedMetadataTransformer';
 import { SourceComponent } from '../metadata-registry';
 
 export type ConvertTransactionState = {

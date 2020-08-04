f54aa23aed3555bb2434158458c861f920902e75
William Ruemmele <william.ruemmele@salesforce.com>
2020-08-04 09:02:11 -0600

    replaced gitignore-parser with ignore

--- src/metadata-registry/forceIgnore.ts
+++ src/metadata-registry/forceIgnore.ts
@@ -4,23 +4,31 @@
  * Licensed under the BSD 3-Clause license.
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
-//@ts-ignore
-import * as gitignore from 'gitignore-parser';
+
+import ignore, { Ignore } from 'ignore/index';
 import { sep, relative, join, dirname, basename } from 'path';
 import { readFileSync } from 'fs';
 import { FORCE_IGNORE_FILE } from '../utils/constants';
 import { SourcePath } from '../types';
 import { searchUp } from '../utils/fileSystemHandler';
 
+const DEFAULT_IGNORE: string[] = [
+  '**/*.dup',
+  '.*',
+  'package2-descriptor.json',
+  'package2-manifest.json',
+];
+
 export class ForceIgnore {
-  /* eslint-disable-next-line  @typescript-eslint/no-explicit-any */
-  private parser: any;
-  private forceIgnoreDirectory: string;
+  private readonly parser: Ignore;
+  private readonly forceIgnoreDirectory: string;
 
   constructor(forceIgnorePath = '') {
     try {
-      const forceIgnoreContents = this.parseContents(readFileSync(forceIgnorePath, 'utf8'));
-      this.parser = gitignore.compile(forceIgnoreContents);
+      const forceIgnoreContents = DEFAULT_IGNORE.concat(
+        this.parseContents(readFileSync(forceIgnorePath, 'utf-8'))
+      );
+      this.parser = ignore().add(forceIgnoreContents);
       this.forceIgnoreDirectory = dirname(forceIgnorePath);
     } catch (e) {
       // TODO: log no force ignore
@@ -46,39 +54,24 @@ export class ForceIgnore {
   public denies(fsPath: SourcePath): boolean {
     let denies = false;
     if (this.parser) {
-      denies = this.parser.denies(relative(this.forceIgnoreDirectory, fsPath));
+      denies = this.parser.ignores(relative(this.forceIgnoreDirectory, fsPath));
     }
-    return denies || !this.isValidAgainstDefaults(fsPath);
+    return denies;
   }
 
   public accepts(fsPath: SourcePath): boolean {
     let accepts = true;
     if (this.parser) {
-      accepts = this.parser.accepts(relative(this.forceIgnoreDirectory, fsPath));
+      accepts = !this.parser.ignores(relative(this.forceIgnoreDirectory, fsPath));
     }
-    return accepts && this.isValidAgainstDefaults(fsPath);
+    return accepts;
   }
 
-  /**
-   * This is not how we should ultimately test a path against default ignores.
-   * TODO: When we utilize a new matching module this should be replaced.
-   */
-  private isValidAgainstDefaults(fsPath: SourcePath): boolean {
-    const name = basename(fsPath);
-    return (
-      !name.startsWith('.') &&
-      !name.endsWith('.dup') &&
-      name !== 'package2-descriptor.json' &&
-      name !== 'package2-manifest.json'
-    );
-  }
-
-  private parseContents(contents: string): string {
+  private parseContents(contents: string): string[] {
     return contents
       .split('\n')
       .map((line) => line.trim())
       .map((line) => line.replace(/[\\\/]/g, sep))
-      .map((line) => line.replace(/^\\/, ''))
-      .join('\n');
+      .map((line) => line.replace(/^\\/, ''));
   }
 }

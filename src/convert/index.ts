a355e7fd428e78c8a7490e7a1044fe44114b6034
Bryan Powell <b.powell@salesforce.com>
2020-08-10 09:53:57 -0700

    migrate convert types to own module (#105)

--- src/convert/index.ts
+++ src/convert/index.ts
@@ -5,3 +5,11 @@
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 export { MetadataConverter } from './metadataConverter';
+export {
+  ConvertResult,
+  ConvertOutputConfig,
+  SfdxFileFormat,
+  WriteInfo,
+  WriterFormat,
+  MetadataTransformer,
+} from './types';

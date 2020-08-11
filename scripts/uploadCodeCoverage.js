32669250660878060b27e9c0a5e84b3977e6d69b
Bryan Powell <b.powell@salesforce.com>
2020-08-11 16:28:43 -0700

    retry 3 times for codecov

---
+++ scripts/uploadCodeCoverage.js
@@ -0,0 +1,22 @@
+const { execSilent } = require('./util');
+const { join } = require('path');
+
+const MAX_ATTEMPTS = 3;
+const COVERAGE_RESULTS = join(__dirname, '..', 'test-results', 'coverage', 'lcov.info');
+
+let attempts = 0;
+do {
+  try {
+    const result = execSilent(`curl -s https://codecov.io/bash | basfeh -s -- -f ${COVERAGE_RESULTS}`);
+    console.log(result.stdout);
+    break;
+  } catch (e) {
+    attempts += 1;
+    let message = `Failed to publish coverage results on attempt ${attempts}`;
+    if (attempts < MAX_ATTEMPTS) {
+      message += ' - trying again...';
+    }
+    console.log(message);
+    console.log(e.message)
+  }
+} while (attempts < MAX_ATTEMPTS);

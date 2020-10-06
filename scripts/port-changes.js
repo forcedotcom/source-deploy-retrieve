5bd107732c2b75449d5cc5059405d0b0945f5a03
Shelby <shelby.holden@salesforce.com>
2020-10-06 17:47:41 -0600

    chore: replace version with publish bump type

--- scripts/port-changes.js
+++ scripts/port-changes.js
@@ -11,11 +11,43 @@ const shell = require('shelljs');
 
 const PR_REGEX = new RegExp(/(\(#\d+\))(\s+\(#\d+\))*$/);
 const COMMIT_REGEX = new RegExp(/^([\da-zA-Z]+)/);
-const RELEASE_REGEX = new RegExp(/^\d{1,2}\.\d{1,2}\.\d/);
 
 const PR_NUM = 'PR_NUM';
 const COMMIT = 'COMMIT';
 const MESSAGE = 'MESSAGE';
+
+function getReleaseVersion() {
+    const releaseType = getReleaseType();
+    const currentVersion = require('../package.json').version;
+    var [version, major, minor, patch] = currentVersion.match(/^(\d+)\.?(\d+)\.?(\*|\d+)$/);
+    switch (releaseType) {
+        case 'major':
+            major = parseInt(major) + 1;
+            break;
+        case 'minor':
+            minor = parseInt(minor) + 1;
+            break;
+        case 'patch':
+            patch = parseInt(patch) + 1;
+            break;
+    }
+    return `${major}.${minor}.${patch}`;
+}
+
+function getReleaseType() {
+    var releaseIndex = process.argv.indexOf('-r');
+    if (!(releaseIndex > -1)) {
+        console.error('Release version type for the port PR is required. Example: \'patch\', \'minor\', or \'major\'');
+        process.exit(-1);
+    }
+    if (!/patch|minor|major/.exec(`${process.argv[releaseIndex + 1]}`)) {
+        console.error(
+            `Invalid release version type '${process.argv[releaseIndex + 1]}'. Expected patch, minor, or major.`
+        );
+        process.exit(-1);
+    }
+    return process.argv[releaseIndex + 1];
+}
 
 function updateBranches(baseBranch, featureBranch) {
     if (ADD_VERBOSE_LOGGING)
@@ -107,21 +139,10 @@ function isTrueDiff(commitMap) {
     }
 }
 
-function getPortBranch(baseBranch) {
+function getPortBranch(baseBranch, version) {
     if (ADD_VERBOSE_LOGGING)
         console.log('\n\nStep 4: Generate the port PR branch based on -r argument');
-    var releaseIndex = process.argv.indexOf('-r');
-    if (!(releaseIndex > -1)) {
-        console.error('Release version for the port PR is required. Example: \'-r 0.0.5\'');
-        process.exit(-1);
-    }
-    if (!RELEASE_REGEX.exec(`${process.argv[releaseIndex + 1]}`)) {
-        console.error(
-            `Invalid release version '${process.argv[releaseIndex + 1]}'. Expected format [x.y.z].`
-        );
-        process.exit(-1);
-    }
-    const result = shell.exec(`git checkout -b portPR-v${process.argv[releaseIndex + 1]} ${baseBranch}`).stderr.toString().trim();
+    const result = shell.exec(`git checkout -b portPR-v${version} ${baseBranch}`).stderr.toString().trim();
     if (result && result.startsWith('fatal')) {
         console.log('\n\nManual review required. Unable to generate port branch.');
         process.exit(-1);
@@ -138,9 +159,10 @@ function getCherryPickCommits(diffList) {
 
 let ADD_VERBOSE_LOGGING = process.argv.indexOf('-v') > -1 ? true : false;
 
+const releaseVersion = getReleaseVersion();
 updateBranches('main', 'develop');
 const diffList = getAllDiffs('main', 'develop');
 const parsedCommits = parseCommits(diffList);
 const filteredDiffList = filterDiffs(parsedCommits);
-getPortBranch('main');
+getPortBranch('main', releaseVersion);
 getCherryPickCommits(filteredDiffList);

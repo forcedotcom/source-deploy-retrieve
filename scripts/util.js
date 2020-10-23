d9e651df5c0f0b732761f70b40f42a571d9a22e7
Bryan Powell <b.powell@salesforce.com>
2020-10-23 13:25:09 -0700

    style: use terminal codes

--- scripts/util.js
+++ scripts/util.js
@@ -2,22 +2,25 @@ const shell = require('shelljs');
 require('shelljs/global');
 
 const terminalCodes = {
-  FgRed: '\x1b[31m',
-  FgWhite: '\x1b[37m',
+  Red: '\\033[31m',
+  LightGrey: '\\033[37m',
+  Bold: '\\033[1m',
+  ResetAll: '\\033[0m',
 };
 
 module.exports = {
   run: (status, f) => {
     let result;
     shell.exec(`printf "🐎 ${status}..."`);
+    const { LightGrey, Bold, ResetAll } = terminalCodes;
     try {
       result = f();
     } catch (e) {
-      shell.exec(`printf "\\r❗️ ${status}...\\033[1m\\033[37mfailed\\033[0m\\n"`);
+      shell.exec(`printf "\\r❗️ ${status}...${Bold}${LightGrey}failed${ResetAll}\\n"`);
       shell.exec(`printf "${e.message}"`);
       process.exit(1);
     }
-    shell.exec(`printf "\\r✅ ${status}...\\033[1m\\033[37mdone\\033[0m\\n"`);
+    shell.exec(`printf "\\r✅ ${status}...${Bold}${LightGrey}done${ResetAll}\\n"`);
     return result;
   },
   execSilent: (command, swallowError) => {

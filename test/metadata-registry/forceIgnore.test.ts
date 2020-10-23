15a560fa9d210e061e4a32f6f3b5dac368649a4e
Chad Carbert <ccarbert@salesforce.com>
2020-10-23 15:33:22 -0400

    fix: forceignore not respecting trailing / on windows (#190)
    Co-authored-by: Willie Ruemmele <willieruemmele@gmail.com>

--- test/metadata-registry/forceIgnore.test.ts
+++ test/metadata-registry/forceIgnore.test.ts
@@ -50,7 +50,9 @@ describe('ForceIgnore', () => {
     readStub.withArgs(forceIgnorePath).returns(testPattern);
     const forceIgnore = new ForceIgnore(forceIgnorePath);
 
-    let expected = {
+    const file = join('some', 'path', '__tests__', 'myTest.x');
+
+    const expected = {
       eventName: 'FORCE_IGNORE_DIFFERENCE',
       content:
         '**/__tests__/**\n**/*.dup\n**/.*\n**/package2-descriptor.json\n**/package2-manifest.json',
@@ -63,26 +65,8 @@ describe('ForceIgnore', () => {
         '**/package2-descriptor.json',
         '**/package2-manifest.json',
       ],
-      file: 'some/path/__tests__/myTest.x',
+      file,
     };
-
-    if (process.platform === 'win32') {
-      expected = {
-        content:
-          '**\\__tests__\\**\n**\\*.dup\n**\\.*\n**\\package2-descriptor.json\n**\\package2-manifest.json',
-        eventName: 'FORCE_IGNORE_DIFFERENCE',
-        file: 'some\\path\\__tests__\\myTest.x',
-        ignoreLines: [
-          '**\\__tests__\\**',
-          '**\\*.dup',
-          '**\\.*',
-          '**\\package2-descriptor.json',
-          '**\\package2-manifest.json',
-        ],
-        newLibraryResults: true,
-        oldLibraryResults: false,
-      };
-    }
 
     const telemetrySpy = env.spy(Lifecycle.prototype, 'emit');
     // @ts-ignore call the private method directly to avoid excessive stubbing
@@ -90,6 +74,22 @@ describe('ForceIgnore', () => {
     expect(telemetrySpy.calledOnce).to.be.true;
     expect(telemetrySpy.args[0][0]).to.equal('telemetry');
     expect(telemetrySpy.args[0][1]).to.deep.equal(expected);
+  });
+
+  it('Should handle forward slashes on windows', () => {
+    const readStub = env.stub(fs, 'readFileSync');
+    readStub.withArgs(forceIgnorePath).returns('force-app/main/default/classes/');
+    const fi = new ForceIgnore(forceIgnorePath);
+    // @ts-ignore private field
+    expect(fi.parser, 'if constructor throws, parser is not defined').to.not.equal(undefined);
+  });
+
+  it('Should have the correct default in the case the parsers are not initialized', () => {
+    const readStub = env.stub(fs, 'readFileSync');
+    readStub.withArgs(forceIgnorePath).returns('force-app/main/default/classes/');
+    const fi = new ForceIgnore(forceIgnorePath);
+    // @ts-ignore private field
+    expect(fi.accepts(join('force-app', 'main', 'default', 'classes'))).to.be.true;
   });
 
   /**

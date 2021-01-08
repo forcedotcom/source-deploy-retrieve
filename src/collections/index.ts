b149174c2ba2d1431a52ae588a90ae8592dd8a21
Bryan Powell <b.powell@salesforce.com>
2021-01-08 14:23:08 -0500

    feat: lazy collections

--- src/collections/index.ts
+++ src/collections/index.ts
@@ -5,4 +5,5 @@
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 export { ComponentSet } from './componentSet';
+export { LazyCollection } from './lazyCollection';
 export { FromSourceOptions, FromManifestOptions, ResolveOptions } from './types';

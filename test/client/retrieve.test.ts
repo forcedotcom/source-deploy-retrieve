dcc56615769b98eb4a76dbe932e8b552920a63d1
Ananya <a.jha@salesforce.com>
2021-02-05 08:49:05 -0800

    chore: bump core to 2.18.2

--- test/client/retrieve.test.ts
+++ test/client/retrieve.test.ts
@@ -61,6 +61,8 @@ describe('Tooling Retrieve', () => {
     $$.setConfigStubContents('AuthInfoConfig', {
       contents: await testData.getConfig(),
     });
+    // Stub retrieveMaxApiVersion to get over "Domain Not Found: The org cannot be found" error
+    sandboxStub.stub(Connection.prototype, 'retrieveMaxApiVersion').resolves('50.0');
     mockConnection = await Connection.create({
       authInfo: await AuthInfo.create({
         username: testData.username,
--- test/client/toolingApi.test.ts
+++ test/client/toolingApi.test.ts
@@ -31,6 +31,8 @@ describe('Tooling API tests', () => {
     $$.setConfigStubContents('AuthInfoConfig', {
       contents: await testData.getConfig(),
     });
+    // Stub retrieveMaxApiVersion to get over "Domain Not Found: The org cannot be found" error
+    sandboxStub.stub(Connection.prototype, 'retrieveMaxApiVersion').resolves('50.0');
     mockConnection = await Connection.create({
       authInfo: await AuthInfo.create({
         username: testData.username,

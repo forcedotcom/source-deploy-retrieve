3174da59b90ededeff0836533a7094bdb5f8210d
Bryan Powell <b.powell@salesforce.com>
2021-02-19 09:08:59 -0800

    refactor: move normalizeToArray to its own file

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -4,14 +4,7 @@
  * Licensed under the BSD 3-Clause license.
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
-
 export { createFiles } from './fileSystemHandler';
 export { generateMetaXML, generateMetaXMLPath, trimMetaXmlSuffix } from './metadata';
 export { extName, baseName } from './path';
-
-export function normalizeToArray<T>(entryOrArray: T | T[] | undefined): T[] {
-  if (entryOrArray) {
-    return Array.isArray(entryOrArray) ? entryOrArray : [entryOrArray];
-  }
-  return [];
-}
+export { normalizeToArray } from './collections';

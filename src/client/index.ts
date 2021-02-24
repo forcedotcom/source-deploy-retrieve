6bd8edad1ed6bc6b1c3039159b2172bfbe254077
Luis Campos <lcamposguajardo@gmail.com>
2021-02-24 12:04:31 -0800

    chore: update missing exported classes & types

--- src/client/index.ts
+++ src/client/index.ts
@@ -4,10 +4,18 @@
  * Licensed under the BSD 3-Clause license.
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
+
 export { MetadataApiDeploy, MetadataApiDeployOptions } from './metadataApiDeploy';
-export { MetadataApiRetrieve, MetadataApiRetrieveOptions } from './metadataApiRetrieve';
+export {
+  MetadataApiRetrieve,
+  MetadataApiRetrieveOptions,
+  RetrieveResult,
+} from './metadataApiRetrieve';
 export { ToolingApi, ToolingDeployOptions, ToolingRetrieveOptions } from './toolingApi';
 export {
+  DeployResult,
+  FileResponse,
+  MetadataApiRetrieveStatus,
   RetrieveOptions,
   SourceDeployResult,
   RetrieveMessage,

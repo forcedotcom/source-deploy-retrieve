7efd4933bafe523afa9907b0ffc4cf0b477da7ff
Willie Ruemmele <willieruemmele@gmail.com>
2021-05-27 14:41:41 -0600

    chore: undo MetadataTransfer cancel

--- src/client/metadataTransfer.ts
+++ src/client/metadataTransfer.ts
@@ -65,8 +65,6 @@ export abstract class MetadataTransfer<
 
   public cancel(): void {
     this.signalCancel = true;
-    // unawaited async method
-    this.doCancel();
   }
 
   public onUpdate(subscriber: (result: Status) => void): void {

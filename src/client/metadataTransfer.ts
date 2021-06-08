f26da78803d0f809d5dced21f6fe704befc61d16
Steve Hetzel <shetzel@salesforce.com>
2021-06-08 16:16:51 -0600

    feat: add support for making asynchronous metadata transfers (#334)
    * feat: add support for making asynchronous metadata transfers
    
    disable polling for metadata transfer responses.  add a timeout feature to the poller.  always emit
    and return a non-undefined response.
    
    * feat: support async metadata transfers from a ComponentSet
    
    Splits MetadataTransfer.start() into a 2 methods; start and pollStatus. Adds the ID from running the
    pre() functions to the respective classes for use by clients. Adds the AsyncResult type and exports
    it.
    
    * feat: async support for deploys and retrieves
    
    BREAKING CHANGE: metadata transfers are now done in 2 steps - start and pollStatus
    
    * refactor: updates for review
    
    make componentSet option optional. update jsdoc. regenerate yarn.lock with yarnkpkg registry.
    
    * refactor(yarn.lock): start with existing yarn.lock
    
    do not regenerate the yarn.lock from scratch; use existing.

--- src/client/metadataTransfer.ts
+++ src/client/metadataTransfer.ts
@@ -4,17 +4,19 @@
  * Licensed under the BSD 3-Clause license.
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
-import { AuthInfo, Connection, fs, Logger } from '@salesforce/core';
+import { AuthInfo, Connection, fs, Logger, PollingClient, StatusResult } from '@salesforce/core';
 import { EventEmitter } from 'events';
 import { ComponentSet } from '../collections';
 import { MetadataTransferError } from '../errors';
-import { MetadataRequestStatus, RequestStatus, MetadataTransferResult } from './types';
+import { AsyncResult, MetadataRequestStatus, RequestStatus, MetadataTransferResult } from './types';
 import { MetadataConverter, SfdxFileFormat } from '../convert';
 import { join } from 'path';
+import { Duration } from '@salesforce/kit';
+import { AnyJson, isNumber } from '@salesforce/ts-types';
 
 export interface MetadataTransferOptions {
   usernameOrConnection: string | Connection;
-  components: ComponentSet;
+  components?: ComponentSet;
   apiVersion?: string;
   id?: string;
 }
@@ -44,31 +46,75 @@ export abstract class MetadataTransfer<
   }
 
   /**
-   * Start the metadata transfer.
+   * Send the metadata transfer request to the org.
    *
-   * @param pollInterval Frequency in milliseconds to poll for operation status
+   * @returns AsyncResult from the deploy or retrieve response.
    */
-  public async start(pollInterval = 100): Promise<Result | undefined> {
+  public async start(): Promise<AsyncResult> {
+    this.canceled = false;
+    const asyncResult = await this.pre();
+    this._id = asyncResult.id;
+    this.logger.debug(`Started metadata transfer. ID = ${this.id}`);
+    return asyncResult;
+  }
+
+  /**
+   * Poll for the status of the metadata transfer request.
+   * Default frequency is 100 ms.
+   * Default timeout is 60 minutes.
+   *
+   * @param options Polling options; frequency, timeout, polling function.
+   * @returns The result of the deploy or retrieve.
+   */
+  public async pollStatus(options?: Partial<PollingClient.Options>): Promise<Result>;
+  /**
+   * Poll for the status of the metadata transfer request.
+   * Default frequency is 100 ms.
+   * Default timeout is 60 minutes.
+   *
+   * @param frequency Polling frequency in milliseconds.
+   * @param timeout Polling timeout in seconds.
+   * @returns The result of the deploy or retrieve.
+   */
+  public async pollStatus(frequency?: number, timeout?: number): Promise<Result>;
+  public async pollStatus(
+    frequencyOrOptions?: number | Partial<PollingClient.Options>,
+    timeout?: number
+  ): Promise<Result> {
+    let pollingOptions: PollingClient.Options = {
+      frequency: Duration.milliseconds(100),
+      timeout: Duration.minutes(60),
+      poll: this.poll.bind(this),
+    };
+    if (isNumber(frequencyOrOptions)) {
+      pollingOptions.frequency = Duration.milliseconds(frequencyOrOptions);
+    } else if (frequencyOrOptions !== undefined) {
+      pollingOptions = { ...pollingOptions, ...frequencyOrOptions };
+    }
+    if (isNumber(timeout)) {
+      pollingOptions.timeout = Duration.seconds(timeout);
+    }
+
+    const pollingClient = await PollingClient.create(pollingOptions);
+
     try {
-      this.canceled = false;
-      const { id } = await this.pre();
-      this._id = id;
-      const apiResult = await this.pollStatus(pollInterval);
-
-      if (!apiResult || apiResult.status === RequestStatus.Canceled) {
-        this.event.emit('cancel', apiResult);
-        return;
+      this.logger.debug(`Polling for metadata transfer status. ID = ${this.id}`);
+      this.logger.debug(`Polling frequency (ms): ${pollingOptions.frequency.milliseconds}`);
+      this.logger.debug(`Polling timeout (min): ${pollingOptions.timeout.minutes}`);
+      const completedMdapiStatus = ((await pollingClient.subscribe()) as unknown) as Status;
+      const result = await this.post(completedMdapiStatus);
+      if (completedMdapiStatus.status === RequestStatus.Canceled) {
+        this.event.emit('cancel', completedMdapiStatus);
+      } else {
+        this.event.emit('finish', result);
       }
-
-      const sourceResult = await this.post(apiResult);
-      this.event.emit('finish', sourceResult);
-
-      return sourceResult;
+      return result;
     } catch (e) {
+      const error = new MetadataTransferError('md_request_fail', e.message);
       if (this.event.listenerCount('error') === 0) {
-        throw e;
+        throw error;
       }
-      this.event.emit('error', e);
+      this.event.emit('error', error);
     }
   }
 
@@ -98,7 +144,7 @@ export abstract class MetadataTransfer<
         `Converting metadata to: ${mdapiTempDir} because the SFDX_MDAPI_TEMP_DIR environment variable is set`
       );
       try {
-        const source = cs || this.components;
+        const source = cs || this.components || new ComponentSet();
         const converter = new MetadataConverter();
         if (target === 'source') {
           await converter.convert(source.getSourceComponents().toArray(), target, {
@@ -133,50 +179,33 @@ export abstract class MetadataTransfer<
     return this.usernameOrConnection;
   }
 
-  private async pollStatus(interval: number): Promise<Status> {
-    let result: Status;
-    let triedOnce = false;
+  private async poll(): Promise<StatusResult> {
+    let completed = false;
+    let mdapiStatus: Status;
 
-    try {
-      while (true) {
-        if (this.canceled) {
-          if (result) {
-            result.status = RequestStatus.Canceled;
-          }
-          return result;
-        }
-
-        if (triedOnce) {
-          await this.wait(interval);
-        }
-
-        result = await this.checkStatus();
-
-        switch (result.status) {
-          case RequestStatus.Canceled:
-            this.canceled = true;
-          case RequestStatus.Succeeded:
-          case RequestStatus.Failed:
-            return result;
-        }
-
-        this.event.emit('update', result);
-
-        triedOnce = true;
+    if (this.canceled) {
+      // This only happens for a canceled retrieve. Canceled deploys are
+      // handled via checkStatus response.
+      if (!mdapiStatus) {
+        mdapiStatus = { id: this.id, success: false, done: true } as Status;
       }
-    } catch (e) {
-      throw new MetadataTransferError('md_request_fail', e.message);
+      mdapiStatus.status = RequestStatus.Canceled;
+      completed = true;
+      this.canceled = false;
+    } else {
+      mdapiStatus = await this.checkStatus();
+      completed = mdapiStatus?.done;
+      if (!completed) {
+        this.event.emit('update', mdapiStatus);
+      }
     }
-  }
+    this.logger.debug(`MDAPI status update: ${mdapiStatus.status}`);
 
-  private wait(interval: number): Promise<void> {
-    return new Promise((resolve) => {
-      setTimeout(resolve, interval);
-    });
+    return { completed, payload: (mdapiStatus as unknown) as AnyJson };
   }
 
   public abstract checkStatus(): Promise<Status>;
   public abstract cancel(): Promise<void>;
-  protected abstract pre(): Promise<{ id: string }>;
+  protected abstract pre(): Promise<AsyncResult>;
   protected abstract post(result: Status): Promise<Result>;
 }

c1b515c613eb23ce7bc329904d36b17872ad5bd9
Violet Yao <vyao@salesforce.com>
2021-08-17 08:53:32 -0700

    refactor: format

--- packages/source-deploy-retrieve/scripts/update-registry/index.js
+++ packages/source-deploy-retrieve/scripts/update-registry/index.js
@@ -5,7 +5,14 @@ const path = require('path');
 const update = require('./update');
 const { run, execSilent } = require('../util');
 
-const REGISTRY_PATH = path.join(__dirname, '..', '..', 'src', 'registry', 'registry.json');
+const REGISTRY_PATH = path.join(
+  __dirname,
+  '..',
+  '..',
+  'src',
+  'registry',
+  'registry.json'
+);
 
 function printHelp() {
   const message = `
@@ -20,9 +27,10 @@ authenticated to beforehand with the Salesforce CLI.
 
 The update process only adds new entries or modifies existing ones. Please manually review
 the changes after they have been generated to ensure there were no unexpected modifications. 
-`;
+`
   console.log(message);
 }
+
 
 const [apiVersion, source, sourceArg] = process.argv.slice(2, 5);
 
@@ -46,7 +54,7 @@ if (source === '-p') {
 run('Applying registry updates', () => {
   const registry = fs.existsSync(REGISTRY_PATH)
     ? JSON.parse(fs.readFileSync(REGISTRY_PATH))
-    : { types: {}, suffixes: {}, strictTypeFolder: {} };
+    : { types: {}, suffixes: {}, strictTypeFolder: {} };;
 
   update(registry, describeResult);
   registry.apiVersion = apiVersion;

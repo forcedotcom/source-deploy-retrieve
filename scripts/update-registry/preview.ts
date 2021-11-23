b181f1f1f5aa81a2f337fc4e926d21453dcf6721
mshanemc <shane.mclaughlin@salesforce.com>
2021-11-23 12:28:01 -0600

    feat: ci and slack integration

--- scripts/update-registry/preview.ts
+++ scripts/update-registry/preview.ts
@@ -17,7 +17,52 @@ import { registry } from '../../src';
   const missingTypes = getMissingTypes(nextCoverage, registry).map((type) => type[0]);
 
   console.log(`There are ${missingTypes.length} new types for v${nextCoverage.apiVersion} not in the registry.`);
-  console.log(
-    `${missingTypes.map((t) => `${t} (${nextCoverage.types[t].orgShapes.developer.features.join(';')})`).join('\n')}`
-  );
+  // console.log(
+  //   `${missingTypes.map((t) => `${t} (${nextCoverage.types[t].orgShapes.developer.features.join(';')})`).join('\n')}`
+  // );
+
+  const typesByFeature = new Map<string, string[]>();
+  missingTypes.map((t) => {
+    const featureLabel = nextCoverage.types[t].orgShapes.developer.features?.join(' & ') ?? 'NO FEATURE REQUIRED';
+    typesByFeature.set(featureLabel, [...(typesByFeature.get(featureLabel) ?? []), t]);
+  });
+  console.log(typesByFeature);
+  const formattedTypes: string[] = [];
+  typesByFeature.forEach((types, feature) => {
+    formattedTypes.push(`*${feature}*\n  -  ${types.join(', ')}`);
+  });
+
+  const json = {
+    blocks: [
+      {
+        type: 'header',
+        text: {
+          type: 'plain_text',
+          text: `v${nextCoverage.apiVersion} Metadata Preview`,
+        },
+      },
+      {
+        type: 'section',
+        text: {
+          type: 'plain_text',
+          text: `There are ${missingTypes.length} new types not in the registry, organized by required features (if any).  Slack can show a max of 50.`,
+        },
+      },
+      {
+        type: 'section',
+        text: {
+          type: 'mrkdwn',
+          text: formattedTypes.join('\n\n'),
+        },
+      },
+    ],
+  };
+  // console.log(JSON.stringify(json, null, 2));
+  try {
+    await got.post(process.env.DEFAULT_SLACK_WEBHOOK, {
+      json,
+    });
+  } catch (e) {
+    console.error(e);
+  }
 })();

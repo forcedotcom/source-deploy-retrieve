1eac718d573b38c034f9c37fe780ffe6defb73a7
Willie Ruemmele <willieruemmele@gmail.com>
2022-06-08 15:36:24 -0600

    fix: allow larger directories to be compressed, test in plugin-source

--- src/client/metadataApiDeploy.ts
+++ src/client/metadataApiDeploy.ts
@@ -409,7 +409,7 @@ export class MetadataApiDeploy extends MetadataTransfer<MetadataApiDeployStatus,
       const zip = createArchive('zip', { zlib: { level: 9 } });
       // anywhere not at the root level is fine
       zip.directory(this.options.mdapiPath, 'zip');
-      await zip.finalize();
+      void zip.finalize();
       return stream2buffer(zip);
     }
     // read the zip into a buffer

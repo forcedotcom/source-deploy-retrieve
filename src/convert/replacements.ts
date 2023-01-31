e5a30dc9916e65d7bd7cc3478450b932ea7cbb59
Cristian Dominguez <cdominguez@salesforce.com>
2023-01-31 16:01:19 -0300

    fix: pass output dir to SfProject instance

--- src/convert/replacements.ts
+++ src/convert/replacements.ts
@@ -81,9 +81,9 @@ export const replacementIterations = async (input: string, replacements: MarkedR
 /**
  * Reads the project, gets replacements, removes any that aren't applicable due to environment conditionals, and returns an instance of the ReplacementMarkingStream
  */
-export const getReplacementMarkingStream = async (): Promise<ReplacementMarkingStream | undefined> => {
+export const getReplacementMarkingStream = async (path: string): Promise<ReplacementMarkingStream | undefined> => {
   // remove any that don't agree with current env
-  const filteredReplacements = envFilter(await readReplacementsFromProject());
+  const filteredReplacements = envFilter(await readReplacementsFromProject(path));
   if (filteredReplacements.length) {
     return new ReplacementMarkingStream(filteredReplacements);
   }
@@ -207,8 +207,8 @@ const getEnvValue = (env: string): string => {
 /**
  * Read the `replacement` property from sfdx-project.json
  */
-const readReplacementsFromProject = async (): Promise<ReplacementConfig[]> => {
-  const proj = await SfProject.resolve();
+const readReplacementsFromProject = async (path: string): Promise<ReplacementConfig[]> => {
+  const proj = await SfProject.resolve(path);
   const projJson = (await proj.resolveProjectConfig()) as { replacements?: ReplacementConfig[] };
   return projJson.replacements;
 };

00c437662878aaadee74a2f9a0fce998dab68813
Prashanth Josyula <prashanth.16@gmail.com>
2023-03-13 19:09:23 +0530

    fix(mixedcontentsourceadapter.test.ts): error during delete operation of experiencepropertytype
    This change is to fix the error during the delete operation for experiencepropertytype metadata
    bundle. As the bundle is all JSON files and there is no XML file in the bundle there will be an
    error when the delete operation fails in the backend and this is the fix for the same.

---
+++ test/mock/type-constants/experiencePropertyTypeBundleConstants.ts
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2020, salesforce.com, inc.
+ * All rights reserved.
+ * Licensed under the BSD 3-Clause license.
+ * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+import { join } from 'path';
+
+import { registry, SourceComponent } from '../../../src';
+
+/** Experience Property Type Bundle will be of the following shape.
+experiencePropertyTypeBundles/
+prop1/
+schema.json
+design.json
+ 
+ schema.json is always expected to be in the bundle, while design.json is optional.
+ 
+ Do note that we dont have any -meta.xml. We can consider schema.json as our meta XML file.
+ */
+
+// This is the type defined in metadataRegistry.json
+const type = registry.types.experiencepropertytypebundle;
+
+// This will be the root directory experiencePropertyTypeBundles.It is something like /path/to/experiencePropertyTypeBundles
+export const TYPE_DIRECTORY = join('path', 'to', type.directoryName);
+
+// This is the name of the property type we are creating.
+export const COMPONENT_NAME = 'prop1';
+
+// This is the schema.json and design.json paths inside the property type.
+export const CONTENT_NAMES = [type.metaFileSuffix, 'design.json'];
+
+// This is the complete path to the content. It is something like /path/to/experiencePropertyTypeBundles/prop1/schema.json.
+export const CONTENT_PATHS = CONTENT_NAMES.map((n) => join(TYPE_DIRECTORY, join(COMPONENT_NAME, n)));
+
+// Finally we construct our component.
+export const COMPONENT = SourceComponent.createVirtualComponent(
+  {
+    name: COMPONENT_NAME,
+    type,
+    content: join(TYPE_DIRECTORY, COMPONENT_NAME),
+    xml: CONTENT_PATHS[0],
+  },
+  [
+    {
+      dirPath: TYPE_DIRECTORY,
+      children: [COMPONENT_NAME],
+    },
+  ]
+);

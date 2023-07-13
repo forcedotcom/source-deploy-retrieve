916be6ef2e67e91929bbd2b6f3f98224460a7c68
mshanemc <shane.mclaughlin@salesforce.com>
2023-07-13 09:56:32 -0500

    feat: new mdType ProcessFlowMigration

--- src/registry/nonSupportedTypes.ts
+++ src/registry/nonSupportedTypes.ts
@@ -87,9 +87,6 @@ export const metadataTypes = [
   // spun up org with ASSOCIATIONENGINE, not in describe
   'RecAlrtDataSrcExpSetDef',
   'RecordAlertTemplate',
-
-  // does not show up in describe, but requires no features
-  'ProcessFlowMigration',
 ];
 
 export const hasUnsupportedFeatures = (type: CoverageObjectType): boolean => {

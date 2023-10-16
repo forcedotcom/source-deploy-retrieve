7fe0bab1d6815afa3daad7ed0a3cac4f5808ea20
Steve Hetzel <shetzel@salesforce.com>
2023-10-16 15:51:43 -0600

    fix: component set maps treat encoded and decoded keys as the same (#1138)
    * feat: component set components are now DecodableMaps
    
    * fix: move DecodeableMap to a new file
    
    * chore: add tests for DecodeableMap
    
    * fix: use internal map of decoded to encoded keys
    
    * chore: update comment for internal map initialization

---
+++ test/collections/decodeableMap.test.ts
@@ -0,0 +1,241 @@
+/*
+ * Copyright (c) 2020, salesforce.com, inc.
+ * All rights reserved.
+ * Licensed under the BSD 3-Clause license.
+ * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+import { expect } from 'chai';
+import * as sinon from 'sinon';
+import { DecodeableMap } from '../../src/collections/decodeableMap';
+
+describe('DecodeableMap', () => {
+  let dMap: DecodeableMap<string, string>;
+  const layout1_key_encoded = 'Layout-v1%2E1 Layout';
+  const layout1_key_decoded = 'Layout-v1.1 Layout';
+  const layout1_value = 'layout1.1-value';
+  const layout9_key_encoded = 'Layout-v9%2E2 Layout';
+  const layout9_key_decoded = 'Layout-v9.2 Layout';
+  const layout9_value = 'layout9.2-value';
+  const nonExistent_key_decoded = 'Layout-v3.3-MISSING Layout';
+  const nonExistent_key_encoded = 'Layout-v3%2E3-MISSING Layout';
+
+  const sandbox = sinon.createSandbox();
+  let hasMapSpy: sinon.SinonSpy;
+  let getMapSpy: sinon.SinonSpy;
+  let setMapSpy: sinon.SinonSpy;
+  let deleteMapSpy: sinon.SinonSpy;
+
+  beforeEach(() => {
+    dMap = new DecodeableMap([
+      [layout1_key_encoded, layout1_value],
+      [layout9_key_decoded, layout9_value],
+    ]);
+    hasMapSpy = sandbox.spy(Map.prototype, 'has');
+    getMapSpy = sandbox.spy(Map.prototype, 'get');
+    setMapSpy = sandbox.spy(Map.prototype, 'set');
+    deleteMapSpy = sandbox.spy(Map.prototype, 'delete');
+  });
+
+  afterEach(() => {
+    sandbox.restore();
+  });
+
+  describe('has()', () => {
+    it('should match an encoded key with a decoded key', () => {
+      expect(dMap.has(layout1_key_decoded)).to.be.true;
+      expect(hasMapSpy.calledWith(layout1_key_decoded)).to.be.true;
+      expect(hasMapSpy.calledWith(layout1_key_encoded)).to.be.true;
+    });
+
+    it('should match an encoded key with an encoded key', () => {
+      expect(dMap.has(layout1_key_encoded)).to.be.true;
+      expect(hasMapSpy.calledWith(layout1_key_encoded)).to.be.true;
+      expect(hasMapSpy.calledWith(layout1_key_decoded)).to.be.false;
+    });
+
+    it('should match a decoded key with a decoded key', () => {
+      expect(dMap.has(layout9_key_decoded)).to.be.true;
+      expect(hasMapSpy.calledWith(layout9_key_decoded)).to.be.true;
+      expect(hasMapSpy.calledWith(layout9_key_encoded)).to.be.false;
+    });
+
+    it('should match a decoded key with an encoded key', () => {
+      expect(dMap.has(layout9_key_encoded)).to.be.true;
+      expect(hasMapSpy.calledWith(layout9_key_encoded)).to.be.true;
+      expect(hasMapSpy.calledWith(layout9_key_decoded)).to.be.true;
+    });
+
+    it('should not match on decoded nonexistent key', () => {
+      expect(dMap.has(nonExistent_key_decoded)).to.be.false;
+      expect(hasMapSpy.calledWith(nonExistent_key_decoded)).to.be.true;
+      expect(hasMapSpy.calledWith(nonExistent_key_encoded)).to.be.false;
+    });
+
+    it('should not match on encoded nonexistent key', () => {
+      expect(dMap.has(nonExistent_key_encoded)).to.be.false;
+      expect(hasMapSpy.calledWith(nonExistent_key_encoded)).to.be.true;
+      expect(hasMapSpy.calledWith(nonExistent_key_decoded)).to.be.true;
+    });
+  });
+
+  describe('get()', () => {
+    it('should get value from an encoded key with a decoded key', () => {
+      expect(dMap.get(layout1_key_decoded)).to.equal(layout1_value);
+      expect(getMapSpy.calledWith(layout1_key_decoded)).to.be.true;
+      expect(getMapSpy.calledWith(layout1_key_encoded)).to.be.true;
+    });
+
+    it('should get value from an encoded key with an encoded key', () => {
+      expect(dMap.get(layout1_key_encoded)).to.equal(layout1_value);
+      expect(getMapSpy.calledWith(layout1_key_encoded)).to.be.true;
+      expect(getMapSpy.calledWith(layout1_key_decoded)).to.be.false;
+    });
+
+    it('should get value from a decoded key with a decoded key', () => {
+      expect(dMap.get(layout9_key_decoded)).to.equal(layout9_value);
+      expect(getMapSpy.calledWith(layout9_key_decoded)).to.be.true;
+      expect(getMapSpy.calledWith(layout9_key_encoded)).to.be.false;
+    });
+
+    it('should get value from a decoded key with an encoded key', () => {
+      expect(dMap.get(layout9_key_encoded)).to.equal(layout9_value);
+      expect(getMapSpy.calledWith(layout9_key_encoded)).to.be.false;
+      expect(getMapSpy.calledWith(layout9_key_decoded)).to.be.true;
+    });
+
+    it('should return undefined on decoded nonexistent key', () => {
+      expect(dMap.get(nonExistent_key_decoded)).to.be.undefined;
+      // This is true since it gets from the internal map.
+      expect(getMapSpy.calledWith(nonExistent_key_decoded)).to.be.true;
+      expect(getMapSpy.calledWith(nonExistent_key_encoded)).to.be.false;
+    });
+
+    it('should return undefined on encoded nonexistent key', () => {
+      expect(dMap.get(nonExistent_key_encoded)).to.be.undefined;
+      expect(getMapSpy.calledWith(nonExistent_key_encoded)).to.be.false;
+      expect(getMapSpy.calledWith(nonExistent_key_decoded)).to.be.false;
+    });
+  });
+
+  describe('set()', () => {
+    const NEW_VALUE = 'new value from set';
+
+    it('should update value of decoded key using decoded key', () => {
+      expect(dMap.set(layout9_key_decoded, NEW_VALUE)).to.equal(dMap);
+      expect(setMapSpy.called).to.be.true;
+      expect(setMapSpy.lastCall.args[0]).to.equal(layout9_key_decoded);
+      expect(setMapSpy.lastCall.args[1]).to.equal(NEW_VALUE);
+      expect(dMap.size).to.equal(2);
+      expect(dMap.get(layout9_key_decoded)).to.equal(NEW_VALUE);
+      // @ts-ignore testing private map. expect 1 for initial map creation
+      expect(dMap.keysMap.size).to.equal(1);
+    });
+
+    it('should update value of decoded key using encoded key', () => {
+      expect(dMap.set(layout9_key_encoded, NEW_VALUE)).to.equal(dMap);
+      expect(setMapSpy.called).to.be.true;
+      expect(setMapSpy.lastCall.args[0]).to.equal(layout9_key_decoded);
+      expect(setMapSpy.lastCall.args[1]).to.equal(NEW_VALUE);
+      expect(dMap.size).to.equal(2);
+      expect(dMap.get(layout9_key_encoded)).to.equal(NEW_VALUE);
+      // @ts-ignore testing private map. expect 2 for initial map creation and addition
+      expect(dMap.keysMap.size).to.equal(2);
+      // @ts-ignore testing private map
+      expect(dMap.keysMap.get(layout9_key_decoded)).to.equal(layout9_key_encoded);
+    });
+
+    it('should update value of encoded key using encoded key', () => {
+      expect(dMap.set(layout1_key_encoded, NEW_VALUE)).to.equal(dMap);
+      expect(setMapSpy.called).to.be.true;
+      expect(setMapSpy.lastCall.args[0]).to.equal(layout1_key_encoded);
+      expect(setMapSpy.lastCall.args[1]).to.equal(NEW_VALUE);
+      expect(dMap.size).to.equal(2);
+      expect(dMap.get(layout1_key_encoded)).to.equal(NEW_VALUE);
+      // @ts-ignore testing private map. expect 1 for initial map creation
+      expect(dMap.keysMap.size).to.equal(1);
+    });
+
+    it('should update value of encoded key using decoded key', () => {
+      expect(dMap.set(layout1_key_decoded, NEW_VALUE)).to.equal(dMap);
+      expect(setMapSpy.called).to.be.true;
+      expect(setMapSpy.lastCall.args[0]).to.equal(layout1_key_encoded);
+      expect(setMapSpy.lastCall.args[1]).to.equal(NEW_VALUE);
+      expect(dMap.size).to.equal(2);
+      expect(dMap.get(layout1_key_encoded)).to.equal(NEW_VALUE);
+      // @ts-ignore testing private map. expect 1 for initial map creation
+      expect(dMap.keysMap.size).to.equal(1);
+    });
+
+    it('should set new entry on decoded nonexistent key', () => {
+      expect(dMap.set(nonExistent_key_decoded, NEW_VALUE)).to.equal(dMap);
+      expect(setMapSpy.called).to.be.true;
+      expect(setMapSpy.lastCall.args[0]).to.equal(nonExistent_key_decoded);
+      expect(setMapSpy.lastCall.args[1]).to.equal(NEW_VALUE);
+      expect(dMap.size).to.equal(3);
+      expect(dMap.get(nonExistent_key_decoded)).to.equal(NEW_VALUE);
+      // @ts-ignore testing private map. expect 1 for initial map creation
+      expect(dMap.keysMap.size).to.equal(1);
+    });
+
+    it('should set new entry on encoded nonexistent key', () => {
+      expect(dMap.set(nonExistent_key_encoded, NEW_VALUE)).to.equal(dMap);
+      expect(setMapSpy.called).to.be.true;
+      expect(setMapSpy.lastCall.args[0]).to.equal(nonExistent_key_encoded);
+      expect(setMapSpy.lastCall.args[1]).to.equal(NEW_VALUE);
+      expect(dMap.size).to.equal(3);
+      expect(dMap.get(nonExistent_key_encoded)).to.equal(NEW_VALUE);
+      // @ts-ignore testing private map. expect 2 for initial map creation and addition
+      expect(dMap.keysMap.size).to.equal(2);
+      // @ts-ignore testing private map
+      expect(dMap.keysMap.get(nonExistent_key_decoded)).to.equal(nonExistent_key_encoded);
+    });
+  });
+
+  describe('delete()', () => {
+    it('should delete an encoded key with a decoded key', () => {
+      expect(dMap.delete(layout1_key_decoded)).to.be.true;
+      expect(deleteMapSpy.calledOnce).to.be.true;
+      expect(deleteMapSpy.firstCall.args[0]).to.equal(layout1_key_encoded);
+      expect(dMap.size).to.equal(1);
+      expect(dMap.has(layout1_key_decoded)).to.be.false;
+    });
+
+    it('should delete an encoded key with an encoded key', () => {
+      expect(dMap.delete(layout1_key_encoded)).to.be.true;
+      expect(deleteMapSpy.calledOnce).to.be.true;
+      expect(deleteMapSpy.firstCall.args[0]).to.equal(layout1_key_encoded);
+      expect(dMap.size).to.equal(1);
+      expect(dMap.has(layout1_key_encoded)).to.be.false;
+    });
+
+    it('should delete a decoded key with a decoded key', () => {
+      expect(dMap.delete(layout9_key_decoded)).to.be.true;
+      expect(deleteMapSpy.calledOnce).to.be.true;
+      expect(deleteMapSpy.firstCall.args[0]).to.equal(layout9_key_decoded);
+      expect(dMap.size).to.equal(1);
+      expect(dMap.has(layout9_key_decoded)).to.be.false;
+    });
+
+    it('should delete a decoded key with an encoded key', () => {
+      expect(dMap.delete(layout9_key_encoded)).to.be.true;
+      expect(deleteMapSpy.calledOnce).to.be.true;
+      expect(deleteMapSpy.firstCall.args[0]).to.equal(layout9_key_decoded);
+      expect(dMap.size).to.equal(1);
+      expect(dMap.has(layout9_key_encoded)).to.be.false;
+    });
+
+    it('should not delete a decoded nonexistent key', () => {
+      expect(dMap.delete(nonExistent_key_decoded)).to.be.false;
+      expect(deleteMapSpy.called).to.be.false;
+      expect(dMap.size).to.equal(2);
+      expect(dMap.has(nonExistent_key_decoded)).to.be.false;
+    });
+
+    it('should not delete an encoded nonexistent key', () => {
+      expect(dMap.delete(nonExistent_key_encoded)).to.be.false;
+      expect(deleteMapSpy.called).to.be.false;
+      expect(dMap.size).to.equal(2);
+      expect(dMap.has(nonExistent_key_encoded)).to.be.false;
+    });
+  });
+});

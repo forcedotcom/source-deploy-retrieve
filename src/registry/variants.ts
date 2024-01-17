1df9dbeda47b5f5b7c54566300343c3f5d88a4dc
mshanemc <shane.mclaughlin@salesforce.com>
2024-01-17 17:23:01 -0600

    chore: log presence of customizations

--- src/registry/variants.ts
+++ src/registry/variants.ts
@@ -4,7 +4,7 @@
  * Licensed under the BSD 3-Clause license.
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
-import { SfProject } from '@salesforce/core';
+import { Logger, SfProject } from '@salesforce/core';
 import { deepFreeze } from '../utils/collections';
 import { MetadataRegistry } from './types';
 import * as registryData from './metadataRegistry.json';
@@ -24,11 +24,20 @@ export const getEffectiveRegistry = (input?: RegistryLoadInput): MetadataRegistr
 
 /** read the project to get additional registry customizations */
 const loadRawRegistryInfo = ({ projectDir }: RegistryLoadInput = {}): MetadataRegistry => {
+  const logger = Logger.childFromRoot('variants');
   try {
     const projJson = SfProject.getInstance(projectDir ?? process.cwd()).getSfProjectJson();
 
     // there might not be any customizations in a project, so we default to the emptyRegistry
     // TODO: return the presets combined with the customizations
+    const customizations = projJson.get<MetadataRegistry>('registryCustomizations');
+    if (customizations) {
+      logger.debug(
+        `found registryCustomizations for types [${Object.keys(customizations.types).join(
+          ','
+        )}] in ${projJson.getPath()}`
+      );
+    }
     return projJson.get<MetadataRegistry>('registryCustomizations') ?? emptyRegistry;
   } catch (e) {
     // there might not be a project at all

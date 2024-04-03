0b3e75fcf4a9a689666672d917a88d889ca82961
Shane McLaughlin <shane.mclaughlin@salesforce.com>
2024-04-03 11:53:56 -0500

    feat: absolute paths for string replacements for out-of-project (#1239)
    * feat: absolute paths for string replacements for out-of-project
    
    * test: refactor zip extraction
    
    * feat: componentSetBuilder sets projectDirectory
    
    * fix: tests use relative paths for file replacements, tighter customLabels exception
    
    * fix: registry bug for affinityScoreDefinition
    
    * refactor: variable rename for readability
    
    * test: more ut for whenEnv and absolute files

--- test/nuts/local/replacements/replacementsOutsideProject.nut.ts
+++ test/nuts/local/replacements/replacementsOutsideProject.nut.ts
@@ -8,23 +8,34 @@ import * as path from 'node:path';
 import * as fs from 'node:fs';
 import * as JSZip from 'jszip';
 import { TestSession } from '@salesforce/cli-plugins-testkit';
-import { assert, expect } from 'chai';
+import { assert, expect, config } from 'chai';
+import { logFn } from '@salesforce/kit';
 import { ComponentSetBuilder, MetadataConverter } from '../../../../src';
 import { extractZip } from './extractZip';
 
-describe('e2e replacements test', () => {
+config.truncateThreshold = 0;
+
+describe('replacements from outside a proj ', () => {
   let session: TestSession;
+  let testDir: string;
 
   before(async () => {
     session = await TestSession.create({
       project: {
-        name: 'replacementsNut',
+        name: 'replacementsOutsideProject',
         sourceDir: path.join('test', 'nuts', 'local', 'replacements', 'testProj'),
       },
       devhubAuthStrategy: 'NONE',
     });
-    // Hack: rewrite the file replacement locations relative to the project
-    const projectJsonPath = path.join(session.project.dir, 'sfdx-project.json');
+
+    // then move the project to a new location
+    testDir = path.join(session.dir, 'testDir');
+    await fs.promises.cp(session.project.dir, testDir, { recursive: true });
+    await fs.promises.rm(session.project.dir, { recursive: true });
+
+    // Hack: rewrite the file replacement locations relative to the moved project
+    const projectJsonPath = path.join(testDir, 'sfdx-project.json');
+    logFn(`projectJsonPath: ${projectJsonPath}`);
     const original = await fs.promises.readFile(projectJsonPath, 'utf8');
     await fs.promises.writeFile(projectJsonPath, original);
   });
@@ -37,18 +48,21 @@ describe('e2e replacements test', () => {
     it('converts a componentSet built from the testProj to a zip', async () => {
       process.env.THE_REPLACEMENT = 'foo';
       const converter = new MetadataConverter();
-      const cs = await ComponentSetBuilder.build({ sourcepath: [path.join(session.project.dir, 'force-app')] });
+      const cs = await ComponentSetBuilder.build({
+        sourcepath: [path.join(testDir, 'force-app')],
+        projectDir: testDir,
+      });
       const { zipBuffer } = await converter.convert(cs, 'metadata', {
         type: 'zip',
       });
       assert(zipBuffer, 'zipBuffer should be defined');
       // extract zip files
-      await extractZip(zipBuffer, path.join(session.project.dir, 'unzipped'));
+      await extractZip(zipBuffer, path.join(testDir, 'unzipped'));
     });
 
     it('class replacements as expected', async () => {
       const classContents = await fs.promises.readFile(
-        path.join(session.project.dir, 'unzipped', 'classes', 'replaceStuff.cls'),
+        path.join(testDir, 'unzipped', 'classes', 'replaceStuff.cls'),
         'utf8'
       );
       expect(classContents).to.not.include('replaceMeWithEnv');
@@ -56,7 +70,7 @@ describe('e2e replacements test', () => {
       expect(classContents).to.not.include('replaceEachOfTheseValuesWithAValueFromTheEnvUsingRegex');
       expect(classContents).to.include('foo');
       expect(classContents).to.include(
-        (await fs.promises.readFile(path.join(session.project.dir, 'replacements.txt'), 'utf8')).trim()
+        (await fs.promises.readFile(path.join(testDir, 'replacements.txt'), 'utf8')).trim()
       );
       expect(classContents).to.include('foo');
 
@@ -65,17 +79,15 @@ describe('e2e replacements test', () => {
     });
     it('decomposed object replacements as expected', async () => {
       const objectContents = await fs.promises.readFile(
-        path.join(session.project.dir, 'unzipped', 'objects', 'TestObj__c.object'),
+        path.join(testDir, 'unzipped', 'objects', 'TestObj__c.object'),
         'utf8'
       );
       expect(objectContents).to.not.include('placeholder');
       expect(objectContents).to.include('foo');
-      expect(objectContents).to.include(
-        (await fs.promises.readFile(path.join(session.project.dir, 'label.txt'), 'utf8')).trim()
-      );
+      expect(objectContents).to.include((await fs.promises.readFile(path.join(testDir, 'label.txt'), 'utf8')).trim());
     });
     it('static resource object replacements as expected', async () => {
-      const srZipPath = path.join(session.project.dir, 'unzipped', 'staticresources', 'Test.resource');
+      const srZipPath = path.join(testDir, 'unzipped', 'staticresources', 'Test.resource');
       expect(fs.existsSync(srZipPath)).to.be.true;
       const srZip = await JSZip.loadAsync(fs.readFileSync(srZipPath));
 

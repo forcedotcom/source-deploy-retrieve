151e7ce74f1a3a667049658a5c92dbed64c14906
mingxuanzhang <mingxuanzhang@salesforce.com>
2024-10-30 17:07:25 -0700

    ci: checks for bundling

--- src/registry/registry.ts
+++ src/registry/registry.ts
@@ -5,6 +5,8 @@
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 import { deepFreeze } from '../utils/collections';
+// The static import of json file should never be changed,
+// other read methods might make esbuild fail to bundle the json file
 import * as registryData from './metadataRegistry.json';
 import { MetadataRegistry } from './types';
 

648551b19ae5225256390ee6144a6186ccc5f0d9
Cristian Dominguez <cdominguez@salesforce.com>
2024-11-04 17:15:38 -0300

    fix: check stric types for suffix

--- src/resolve/metadataResolver.ts
+++ src/resolve/metadataResolver.ts
@@ -373,7 +373,16 @@ const parseAsContentMetadataXml =
     const suffixType = registry.getTypeBySuffix(extName(fsPath));
     if (!suffixType) return false;
 
-    return fsPath.split(sep).includes(suffixType.directoryName);
+    const matchesSuffixType = fsPath.split(sep).includes(suffixType.directoryName);
+    if (matchesSuffixType) return matchesSuffixType;
+
+    // it might be a type that requires strict parent folder name.
+    const strictFolderSuffixType = registry
+      .getStrictFolderTypes()
+      .find((l) => l.suffix === suffixType.suffix && l.directoryName && l.name !== suffixType.name);
+    if (!strictFolderSuffixType) return false;
+
+    return fsPath.split(sep).includes(strictFolderSuffixType.directoryName);
   };
 
 /**

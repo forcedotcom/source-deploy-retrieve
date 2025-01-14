2dfd611ef9803210a42ddaef7e219442e67623d3
peternhale <peter.hale@salesforce.com>
2025-01-14 13:24:54 -0700

    chore: wip

--- src/resolve/adapters/esrSourceAdapter.ts
+++ src/resolve/adapters/esrSourceAdapter.ts
@@ -9,12 +9,28 @@ import { ForceIgnore } from '../forceIgnore';
 import { NodeFSTreeContainer } from '../treeContainers';
 import { MixedContentSourceAdapter } from './mixedContentSourceAdapter';
 
+/**
+ * Adapter for handling External Service Registration (ESR) source components.
+ * Extends the MixedContentSourceAdapter to provide specific functionality for ESR components.
+ *
+ * Given a single ESR component, the adapter will:
+ * - Identify the root metadata xml file
+ * - Identify the content file(s) associated with the component from elements schema and serviceBinding from the xml
+ */
 export class EsrSourceAdapter extends MixedContentSourceAdapter {
+  /**
+   * Creates an instance of EsrSourceAdapter.
+   *
+   * @param {MetadataType} type - The metadata type for the ESR component.
+   * @param {RegistryAccess} [registry=new RegistryAccess()] - The registry access instance.
+   * @param {ForceIgnore} [forceIgnore=new ForceIgnore()] - The force ignore instance.
+   * @param {NodeFSTreeContainer} [tree=new NodeFSTreeContainer()] - The file system tree container.
+   */
   public constructor(
     type: MetadataType,
-    registry = new RegistryAccess(),
-    forceIgnore = new ForceIgnore(),
-    tree = new NodeFSTreeContainer()
+    registry: RegistryAccess = new RegistryAccess(),
+    forceIgnore: ForceIgnore = new ForceIgnore(),
+    tree: NodeFSTreeContainer = new NodeFSTreeContainer()
   ) {
     super(type, registry, forceIgnore, tree);
     this.metadataWithContent = false;

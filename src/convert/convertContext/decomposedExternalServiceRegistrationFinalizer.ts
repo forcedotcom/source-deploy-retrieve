35ad23acda1d667560118ef2b7ba2e5f000fb51a
peternhale <peter.hale@salesforce.com>
2025-01-21 10:36:30 -0700

    chore: wip

--- src/convert/convertContext/decomposedExternalServiceRegistrationFinalizer.ts
+++ src/convert/convertContext/decomposedExternalServiceRegistrationFinalizer.ts
@@ -1,24 +1,38 @@
 /*
- * Copyright (c) 2023, salesforce.com, inc.
+ * Copyright (c) 2025, salesforce.com, inc.
  * All rights reserved.
  * Licensed under the BSD 3-Clause license.
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 import { WriterFormat } from '../types';
 import { MetadataType } from '../../registry';
+import { WriteInfo } from '../types';
+import { SourceComponent } from '../../resolve';
 import { ConvertTransactionFinalizer } from './transactionFinalizer';
 
-type ExternalServiceRegistration = unknown;
+type ExternalServiceRegistrationState = {
+  esrRecords: Array<{ component: SourceComponent; writeInfos: WriteInfo[] }>;
+};
 
-export class DecomposedExternalServiceRegistrationFinalizer extends ConvertTransactionFinalizer<ExternalServiceRegistration> {
+export class DecomposedExternalServiceRegistrationFinalizer extends ConvertTransactionFinalizer<ExternalServiceRegistrationState> {
   /** to support custom presets (the only way this code should get hit at all pass in the type from a transformer that has registry access */
   public externalServiceRegistration?: MetadataType;
-  protected transactionState: ExternalServiceRegistration;
+  public transactionState: ExternalServiceRegistrationState = {
+    esrRecords: [],
+  };
   // eslint-disable-next-line class-methods-use-this
-  protected defaultDir: string | undefined;
+  public defaultDir: string | undefined;
 
   public finalize(defaultDirectory: string | undefined): Promise<WriterFormat[]> {
     this.defaultDir = defaultDirectory;
-    return Promise.resolve([]);
+    const writerFormats: WriterFormat[] = this.transactionState.esrRecords.map((esrRecord) => {
+      const { component, writeInfos } = esrRecord;
+      const fullName = component.fullName ?? '';
+      const outputDir = this.defaultDir ? this.defaultDir : '';
+      const esrFileName = `${fullName}.externalServiceRegistration`;
+      const esrFilePath = `${outputDir}/${esrFileName}`;
+      return { component, writeInfos, output: esrFilePath };
+    });
+    return Promise.resolve(writerFormats);
   }
 }
--- src/convert/transformers/decomposeExternalServiceRegistrationTransformer.ts
+++ src/convert/transformers/decomposeExternalServiceRegistrationTransformer.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024, salesforce.com, inc.
+ * Copyright (c) 2023, salesforce.com, inc.
  * All rights reserved.
  * Licensed under the BSD 3-Clause license.
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
@@ -8,16 +8,16 @@ import * as fs from 'node:fs/promises';
 import * as path from 'node:path';
 import { Readable } from 'node:stream';
 import * as yaml from 'yaml';
+import { XMLBuilder } from 'fast-xml-parser';
+import type { ExternalServiceRegistration } from '@jsforce/jsforce-node/lib/api/metadata/schema';
 import { JsonMap } from '@salesforce/ts-types';
-import { XMLBuilder, XMLParser } from 'fast-xml-parser';
 import { WriteInfo } from '../types';
 import { SourceComponent } from '../../resolve';
+import { DEFAULT_PACKAGE_ROOT_SFDX, META_XML_SUFFIX } from '../../common';
 import { BaseMetadataTransformer } from './baseMetadataTransformer';
 
-export type ESR = JsonMap & {
-  ExternalServiceRegistration: {
-    schema?: string;
-  };
+type ESR = JsonMap & {
+  ExternalServiceRegistration: ExternalServiceRegistration;
 };
 
 export class DecomposeExternalServiceRegistrationTransformer extends BaseMetadataTransformer {
@@ -26,17 +26,22 @@ export class DecomposeExternalServiceRegistrationTransformer extends BaseMetadat
     component: SourceComponent;
     mergeWith?: SourceComponent | undefined;
   }): Promise<WriteInfo[]> {
+    this.context.decomposedExternalServiceRegistration.externalServiceRegistration ??=
+      this.registry.getTypeByName('ExternalServiceRegistration');
     const writeInfos: WriteInfo[] = [];
     const { component } = input;
-    const xmlContent = await component.parseXml<ESR>();
-    const esrContent = xmlContent.ExternalServiceRegistration;
+    const outputDir = path.join(
+      this.getOutputFolder('source', component),
+      this.context.decomposedExternalServiceRegistration.externalServiceRegistration.directoryName
+    );
+    const xmlContent = { ...(await component.parseXml<ESR>()).ExternalServiceRegistration };
 
     // Extract schema content
     // eslint-disable-next-line no-underscore-dangle
-    const schemaContent: string = esrContent.schema ?? '';
+    const schemaContent: string = xmlContent.schema ?? '';
     const schemaExtension = this.getSchemaExtension(schemaContent);
     const schemaFileName = `${component.fullName}.${schemaExtension}`;
-    const schemaFilePath = path.join(this.defaultDirectory ?? '', component.type.directoryName, schemaFileName);
+    const schemaFilePath = path.join(path.dirname(outputDir), schemaFileName);
 
     // Write schema content to file
     writeInfos.push({
@@ -45,11 +50,11 @@ export class DecomposeExternalServiceRegistrationTransformer extends BaseMetadat
     });
 
     // Remove schema content from ESR content
-    delete esrContent.schema;
+    delete xmlContent.schema;
 
     // Write remaining ESR content to file
     const esrFileName = `${component.fullName}.externalServiceRegistration`;
-    const esrFilePath = path.join(this.defaultDirectory ?? '', component.type.directoryName, `${esrFileName}-meta.xml`);
+    const esrFilePath = path.join(path.dirname(outputDir), `${esrFileName}${META_XML_SUFFIX}`);
     const xmlBuilder = new XMLBuilder({
       format: true,
       ignoreAttributes: false,
@@ -59,7 +64,7 @@ export class DecomposeExternalServiceRegistrationTransformer extends BaseMetadat
     });
     writeInfos.push({
       // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-      source: xmlBuilder.build({ ExternalServiceRegistration: esrContent }),
+      source: xmlBuilder.build({ ExternalServiceRegistration: xmlContent }),
       output: esrFilePath,
     });
 
@@ -72,20 +77,14 @@ export class DecomposeExternalServiceRegistrationTransformer extends BaseMetadat
     this.context.decomposedExternalServiceRegistration.externalServiceRegistration ??=
       this.registry.getTypeByName('ExternalServiceRegistration');
     const writeInfos: WriteInfo[] = [];
-    const esrFilePath = this.getOutputFile(component);
-    const esrContent = await fs.readFile(esrFilePath, 'utf8');
-    const xmlParser = new XMLParser({
-      ignoreAttributes: false,
-    });
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-    const esrXml = xmlParser.parse(esrContent);
+    const esrFilePath = component.xml;
+    const esrContent = { ...(await component.parseXml<ESR>()).ExternalServiceRegistration };
 
     // Read schema content from file
     const schemaFileName = `${component.fullName}.yaml`; // or .json based on your logic
-    const schemaFilePath = path.join(path.dirname(esrFilePath) ?? '', schemaFileName);
+    const schemaFilePath = path.join(path.dirname(esrFilePath ?? ''), schemaFileName);
     // Add schema content back to ESR content
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-    esrXml.ExternalServiceRegistration['schema'] = await fs.readFile(schemaFilePath, 'utf8');
+    esrContent.schema = await fs.readFile(schemaFilePath, 'utf8');
     const esrMdApiFilePath = `${path.join(
       this.defaultDirectory ?? '',
       component.type.directoryName,
@@ -101,24 +100,23 @@ export class DecomposeExternalServiceRegistrationTransformer extends BaseMetadat
     });
 
     // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-    const source = xmlBuilder.build(esrXml);
-
-    // Write combined content back to source format
+    const source = xmlBuilder.build({ ExternalServiceRegistration: esrContent });
+    // Write combined content back to md format
     writeInfos.push({
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-      source,
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-argument,@typescript-eslint/no-unsafe-assignment
+      source: Readable.from(Buffer.from(source)),
       output: path.resolve(esrMdApiFilePath),
     });
+    this.context.decomposedExternalServiceRegistration.transactionState.esrRecords.push({ component, writeInfos });
 
-    return writeInfos;
+    return [];
   }
 
-  // eslint-disable-next-line class-methods-use-this
-  private getOutputFile(component: SourceComponent, mergeWith?: SourceComponent): string {
-    if (mergeWith?.xml) {
-      return mergeWith.xml;
-    }
-    return component.xml ?? '';
+  // eslint-disable-next-line class-methods-use-this,@typescript-eslint/no-unused-vars
+  private getOutputFolder(format: string, component: SourceComponent, mergeWith?: SourceComponent): string {
+    const base = format === 'source' ? DEFAULT_PACKAGE_ROOT_SFDX : '';
+    const { type } = mergeWith ?? component;
+    return path.join(base, type.directoryName);
   }
 
   // eslint-disable-next-line class-methods-use-this

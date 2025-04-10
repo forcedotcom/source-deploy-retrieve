b8d50438db9d86425dbd5775ff24b025d3565fe0
Steve Hetzel <shetzel@salesforce.com>
2025-04-10 10:34:06 -0600

    feat: add a utility for resolving API version and sourceApiVersion

--- src/client/apiVersionUtil.ts
+++ src/client/apiVersionUtil.ts
@@ -8,7 +8,15 @@
 import { OptionsOfTextResponseBody } from 'got';
 import got from 'got';
 import { ProxyAgent } from 'proxy-agent';
-import { Connection, Logger, SfError, ConfigAggregator, OrgConfigProperties, SfProject, AuthInfo } from '@salesforce/core';
+import {
+  Connection,
+  Logger,
+  SfError,
+  ConfigAggregator,
+  OrgConfigProperties,
+  SfProject,
+  AuthInfo,
+} from '@salesforce/core';
 import { isString } from '@salesforce/ts-types';
 
 let logger: Logger;
@@ -32,21 +40,22 @@ const getProxiedOptions = (url: string): OptionsOfTextResponseBody => ({
 export type ApiVersionConfig = {
   connection?: Connection;
   username?: string;
-}
+};
 
 export type SourceApiVersionConfig = ApiVersionConfig & {
   sfProject?: SfProject;
   sfProjectDir?: string;
-}
+};
 
 export const resolveApiVersion = async (options: ApiVersionConfig): Promise<string> => {
   // Use config value if defined
   const apiVersionConfig = ConfigAggregator.getValue(OrgConfigProperties.ORG_API_VERSION).value;
   if (isString(apiVersionConfig)) {
+    getLogger().debug(`Resolved API version to ${apiVersionConfig} from config.`);
     return apiVersionConfig;
   }
 
-  // Use max api version of target org
+  // Use max api version of a specific org
   let connection = options.connection;
   if (!connection) {
     if (options.username) {
@@ -56,26 +65,28 @@ export const resolveApiVersion = async (options: ApiVersionConfig): Promise<stri
     }
   }
   if (connection) {
-    return connection.retrieveMaxApiVersion();
+    const maxApiVersion = await connection.retrieveMaxApiVersion();
+    getLogger().debug(`Resolved API version to ${maxApiVersion} from org connection.`);
+    return maxApiVersion;
   }
 
   // Use the current SFDC api version in production
   return `${await getCurrentSfdcApiVersion()}.0`;
-}
+};
 
 // Resolution Order:
 //   1. sourceApiVersion set in sfdx-project.json
 //   2. apiVersion from ConfigAggregator (config files and Env Vars)
 //   3. get the highest apiVersion from the App Exchange org
-//   4. hardcoded value of "60.0" as a last resort
 
-export const resolveSourceApiVersion = async (options: SourceApiVersionConfig): Promise<string> => {
-  const project = options.sfProject ?? await SfProject.resolve(options.sfProjectDir);
+export const resolveSourceApiVersion = async (options: SourceApiVersionConfig): Promise<string | undefined> => {
+  const project = options.sfProject ?? (await SfProject.resolve(options.sfProjectDir));
   const projectConfig = await project.resolveProjectConfig();
   if (isString(projectConfig?.sourceApiVersion)) {
-    return projectConfig?.sourceApiVersion;
+    getLogger().debug(`Resolved sourceApiVersion to ${projectConfig.sourceApiVersion} from sfdx-project.json.`);
+    return projectConfig.sourceApiVersion;
   }
-}
+};
 
 type ApiVersion = {
   label: string;
@@ -86,8 +97,9 @@ type ApiVersion = {
 let apiVer: number;
 /**
  * The current API version in Salesforce production orgs. This uses a REST
- * endpoint for the AppExchange org to get the highest supported API version.
- * 
+ * endpoint for the AppExchange org to get the highest supported API version
+ * currently in production.
+ *
  * @returns The current API version as a number. E.g., 63
  */
 export const getCurrentSfdcApiVersion = async (): Promise<number> => {

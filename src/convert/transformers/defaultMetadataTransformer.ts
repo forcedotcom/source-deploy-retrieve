15e12a187aad98fedf19550bf3a97a39da016f9f
Willie Ruemmele <willieruemmele@gmail.com>
2025-05-05 09:15:28 +0200

    fix: fix import, bump version in pjson

--- src/convert/transformers/defaultMetadataTransformer.ts
+++ src/convert/transformers/defaultMetadataTransformer.ts
@@ -4,9 +4,8 @@
  * Licensed under the BSD 3-Clause license.
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
-import { basename, dirname, join } from 'node:path';
+import { basename, dirname, join, sep } from 'node:path';
 import fs from 'node:fs';
-import { sep } from 'node:path';
 import { Messages } from '@salesforce/core/messages';
 import { Lifecycle } from '@salesforce/core/lifecycle';
 import { SourcePath } from '../../common/types';

2cc8edcc0e56b481a4a6e8d15d1069f9a86457d1
github-action-benchmark <github@users.noreply.github.com>
2025-07-09 14:16:20 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for e5e15ab54883b3a1cff502761215e907beb89e95

--- perf-Windows/data.js
+++ perf-Windows/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1750435097308,
+  "lastUpdate": 1752070580199,
   "repoUrl": "https://github.com/forcedotcom/source-deploy-retrieve",
   "entries": {
     "Benchmark": [
@@ -76507,6 +76507,90 @@ window.BENCHMARK_DATA = {
           {
             "name": "lotsOfClassesOneDir-mdapiToSource-win32",
             "value": 15759,
+            "unit": "ms"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "jjasbir@salesforce.com",
+            "name": "jjasbir",
+            "username": "jjasbir"
+          },
+          "committer": {
+            "email": "jjasbir@salesforce.com",
+            "name": "jjasbir",
+            "username": "jjasbir"
+          },
+          "distinct": true,
+          "id": "e5e15ab54883b3a1cff502761215e907beb89e95",
+          "message": "add new ExternalStoragePrvdConfig to metadata registry",
+          "timestamp": "2025-07-09T16:43:06+05:30",
+          "tree_id": "04ced3bb2a119c9b164eea3d0d0a3df5424b63e7",
+          "url": "https://github.com/forcedotcom/source-deploy-retrieve/commit/e5e15ab54883b3a1cff502761215e907beb89e95"
+        },
+        "date": 1752070566939,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "eda-componentSetCreate-win32",
+            "value": 638,
+            "unit": "ms"
+          },
+          {
+            "name": "eda-sourceToMdapi-win32",
+            "value": 4292,
+            "unit": "ms"
+          },
+          {
+            "name": "eda-sourceToZip-win32",
+            "value": 3160,
+            "unit": "ms"
+          },
+          {
+            "name": "eda-mdapiToSource-win32",
+            "value": 6427,
+            "unit": "ms"
+          },
+          {
+            "name": "lotsOfClasses-componentSetCreate-win32",
+            "value": 1339,
+            "unit": "ms"
+          },
+          {
+            "name": "lotsOfClasses-sourceToMdapi-win32",
+            "value": 9134,
+            "unit": "ms"
+          },
+          {
+            "name": "lotsOfClasses-sourceToZip-win32",
+            "value": 5209,
+            "unit": "ms"
+          },
+          {
+            "name": "lotsOfClasses-mdapiToSource-win32",
+            "value": 9215,
+            "unit": "ms"
+          },
+          {
+            "name": "lotsOfClassesOneDir-componentSetCreate-win32",
+            "value": 2364,
+            "unit": "ms"
+          },
+          {
+            "name": "lotsOfClassesOneDir-sourceToMdapi-win32",
+            "value": 16066,
+            "unit": "ms"
+          },
+          {
+            "name": "lotsOfClassesOneDir-sourceToZip-win32",
+            "value": 9150,
+            "unit": "ms"
+          },
+          {
+            "name": "lotsOfClassesOneDir-mdapiToSource-win32",
+            "value": 16326,
             "unit": "ms"
           }
         ]

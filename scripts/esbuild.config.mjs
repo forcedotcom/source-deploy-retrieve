f6b3a9f487560d01e59bca58f677ec2704369efc
Shane McLaughlin <shane.mclaughlin@salesforce.com>
2025-08-21 15:16:35 -0500

    ci: no bundling (#1610)

--- scripts/esbuild.config.mjs
+++ scripts/esbuild.config.mjs
@@ -3,27 +3,19 @@
  * sdr is bundled directly in salesforcedx-vscode
  * The file is only used to detect any potential risks to esbuild.
  **/
-const { build } = require('esbuild');
+import { build } from 'esbuild';
 
-const sharedConfig = {
+await build({
   bundle: true,
   format: 'cjs',
   platform: 'node',
-  external: [], // The allowlist of dependencies that are not bundle-able
   keepNames: true,
-  plugins: [],
   supported: {
     'dynamic-import': false,
   },
   logOverride: {
     'unsupported-dynamic-import': 'error',
   },
-};
-
-(async () => {
-  await build({
-    ...sharedConfig,
-    entryPoints: ['./lib/src/index.js'],
-    outdir: 'dist',
-  });
-})();
+  entryPoints: ['./lib/src/index.js'],
+  outdir: 'dist',
+});
